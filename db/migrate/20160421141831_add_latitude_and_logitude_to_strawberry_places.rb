8e3133278b27a407b3a5270233e6dbd6dd800109
margaretyjkim <margaret.yj.kim@gmail.com>
2016-04-22 19:32:53 -0400

    install npm initialize-css

--- db/migrate/20160421141831_add_latitude_and_logitude_to_strawberry_places.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLogitudeToStrawberryPlaces < ActiveRecord::Migration
-  def change
-    add_column :strawberry_places, :latitude, :float
-    add_column :strawberry_places, :longitude, :float
-  end
-end
